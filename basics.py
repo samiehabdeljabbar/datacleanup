55ff459cf53f506396d95a609a49b80ed4aef64b
Samih Abdeljabbar <abdels6375@gmail.com>
2023-05-09 15:54:07 -0500

    first commit

---
+++ basics.py
@@ -0,0 +1,7 @@
+# define the function with def
+# than name the function, in this example we used get_choices
+
+def get_choices():
+    #than indent the below to make it apart of the function
+    player_choice = "rock"
+    computer_choice = "paper"
