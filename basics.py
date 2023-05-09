3d38c2b2ee1c45788bdde87ca9ac4a8a6eebfdae
Samih Abdeljabbar <abdels6375@gmail.com>
2023-05-09 16:51:28 -0500

    learning how to use functions again

--- basics.py
+++ basics.py
@@ -7,15 +7,29 @@ import random
 def get_choices():
     #than indent the below to make it apart of the function
     player_choice = input("Enter a choice (rock, paper, scissors)")
-    computer_choice = "paper"
+
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
     choices = {"player":player_choice, "computer":computer_choice}
     #putting a blank line as a choice, just to make it easier to read, the computer does not care
     #indentations do matter, they do care
 
-    return computer_choice
-
+    return choices
 choices = get_choices()
 
 print(choices)
+
+#if statements
+#concatanation
+#else and elif statements
+
+def check_win(player, computer):
+    #below is an fstring
+    print(f"You chose {player} and the computer chose {computer}")
+    if player == computer:
+        return("it is a tie!")
+
+check_win("rock", "paper")
+
 
 
