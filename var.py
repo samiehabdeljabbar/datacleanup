b3cb382e071db5e7de1860db0c1a65e9a003e30e
Samih Abdeljabbar <abdels6375@gmail.com>
2023-05-09 20:00:31 -0500

    learning more about variables, and continuing my refresher course

---
+++ var.py
@@ -0,0 +1,24 @@
+name = "Samieh"
+age  = 39
+
+# variables cannot start with a number
+# you cannot use keywords as the start of variables
+
+#an expession is any type of code that returns a value
+
+#indentation matters very much in PYTHON!!!
+
+# Data Types
+
+# String.
+# A String is anythign that is in quotation marks
+
+name = "samieh"
+
+# can print out string using the type function
+print(type(name))
+
+# also can use isinstance() function
+
+age = 25
+print(isinstance(age, int))
